912f9dc017996d67b8a3fc39bf3ad00deb805254
admin <admin@MacBook-Air-admin.local>
2025-11-10 15:27:39 +0300

    chore: install official Redux package and integrate it into the project (replaced custom implementation)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import {createStore} from "./createStore";
+import {createStore} from "redux";
 import {rootReducer} from "./redux/rootReducer";
 import {decrement, increment} from "./redux/actions";
 
